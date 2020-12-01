615e167f86f6fecc8462231c5dc3af677d0c600a
Grayson Schaer <gschaer2@illinois.edu>
2020-12-01 09:20:56 -0600

    Implemented cure kinetics

---
+++ 1D_Cure/Finite_Element_Solver_1D.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 25 09:39:40 2020
+
+@author: Grayson Schaer
+"""
+
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+class FES():
+    
+    def __init__(self):
+    
+        # Simulation parameters
+        self.spacial_precision = 300 # Must be multiple of 10
+        self.temporal_precision = 0.05
+        self.field_length = 0.10
+        self.simulation_time = 300.0
+        self.current_time = 0.0
+        
+        # Initial conditions
+        self.initial_temperature = 298.15
+        self.initial_cure = 0.10
+        self.initial_input_location = self.field_length / 2.0
+        
+        # Boundary conditions
+        self.bc_thermal_conductivity = 0.80
+        self.bc_heat_transfer_coef = 8.59
+        self.ambient_temperature = 298.15
+        
+        # Reward and training targets
+        self.maximum_temperature = 523.15
+        self.desired_front_rate = 0.001
+        
+        # Trigger conditions
+        self.trigger_temperature = 453.15
+        self.trigger_time = 0.0
+        self.trigger_length = 30.0
+        
+        # Physical parameters
+        self.thermal_conductivity = 0.152
+        self.density = 980.0
+        self.enthalpy_of_reaction = 352100.0
+        self.specific_heat = 1440.0
+        self.pre_exponential = 190985.325
+        self.activiation_energy = 51100.0
+        self.gas_const = 8.3144
+        self.model_fit_order = 1.927
+        self.autocatalysis_const = 0.365
+        
+        # Input parameters
+        self.peak_thermal_rate = 5.0
+        self.radius_of_input = self.field_length / 10.0
+        self.input_location = self.initial_input_location
+        self.max_movement_rate = self.field_length * self.temporal_precision
+        self.input_magnitude = self.peak_thermal_rate / 2.0
+        self.max_magnitude_rate = self.peak_thermal_rate * self.temporal_precision
+        K = (self.peak_thermal_rate * self.radius_of_input * np.sqrt(2.0*np.pi)) / (2.0*np.sqrt(np.log(10.0)))
+        sigma = self.radius_of_input / (2.0*np.sqrt(np.log(10.0)))
+        self.front_const = K / (sigma * np.sqrt(2.0 * np.pi))
+        self.exponential_const = -1.0 / (2.0 * sigma * sigma)
+        
+        # Reward constants
+        self.c1 =  5e6
+        self.c2 = 0.20
+        self.max_reward = 5.0
+        self.min_positive_reward = -self.c2 * self.max_reward
+        
+        # Mesh grids
+        self.spacial_grid = np.linspace(0.0,self.field_length,self.spacial_precision)
+        self.temperature_grid = np.array([self.initial_temperature]*self.spacial_precision)
+        self.cure_grid = np.array([self.initial_cure]*self.spacial_precision)
+        self.input_grid = np.array([0.0]*self.spacial_precision)
+        self.front_position = 0.0
+        self.front_rate = 0.0
+        self.previous_front_move = 0.0
+
+    def step(self, action):
+        
+        # Clip the action and use it to update the input's position and magnitude
+        ok_action = True
+        next_input_location = self.input_location + np.clip(action[0], -self.max_movement_rate, self.max_movement_rate)
+        next_magnitude = self.input_magnitude + np.clip(action[1], -self.max_magnitude_rate, self.max_magnitude_rate)
+        if (next_input_location > self.field_length+self.radius_of_input) or (next_input_location < self.radius_of_input):
+            ok_action = False
+        else:
+            self.input_location = next_input_location
+            self.input_magnitude = np.clip(next_magnitude, 0.0, self.peak_thermal_rate)
+            
+        # Update the input grid to reflet the current action
+        self.input_grid = self.input_magnitude * self.front_const * np.exp((self.spacial_grid - self.input_location)**2 * self.exponential_const)
+        self.input_grid[self.input_grid<0.01*self.peak_thermal_rate] = 0.0
+        
+        # Get the second spacial derivative of the temperature field
+        temperature_grid_spline = UnivariateSpline(self.spacial_grid, self.temperature_grid, k=3, s=0.0)
+        temperature_grid_second_derivative = temperature_grid_spline.derivative(n=2)
+        temperature_grid_second_derivative = temperature_grid_second_derivative(self.spacial_grid)
+        
+        # Get the cure rate based on the cure kinetics
+        cure_rate = ((self.pre_exponential*np.exp(-self.activiation_energy / (self.temperature_grid*self.gas_const))) * 
+                     ((1 - self.cure_grid)**self.model_fit_order) * 
+                     (1 + self.autocatalysis_const * self.cure_grid))
+        
+        # Update the cure field using forward Euler method
+        self.cure_grid = self.cure_grid + cure_rate * self.temporal_precision
+        
+        # Calculate the front position and rate
+        if (self.cure_grid >= 0.99).any():
+            new_front_position = self.spacial_grid[np.flatnonzero(self.cure_grid>=0.99)[-1]]
+            if new_front_position != self.front_position:
+                self.front_rate = (new_front_position - self.front_position) / (self.current_time - self.previous_front_move)
+                self.previous_front_move = self.current_time
+        else:
+            new_front_position = 0.0
+            self.front_rate = 0.0
+        self.front_position = new_front_position
+        
+        # Use the second spacial derivative of the temperature field, input field, and cure rate to calculate the temperature field rate
+        temperature_grid_rate = (((self.thermal_conductivity * temperature_grid_second_derivative) + 
+                                  (self.density * self.enthalpy_of_reaction * cure_rate) + 
+                                  (self.density * self.specific_heat * self.input_grid)) / 
+                                  (self.density * self.specific_heat))
+        
+        # Appy the boundary conditions
+        temperature_grid_rate[0] = self.bc_heat_transfer_coef * (self.temperature_grid[0] - self.ambient_temperature) / (-1.0 * self.bc_thermal_conductivity)
+        temperature_grid_rate[-1] = self.bc_heat_transfer_coef * (self.temperature_grid[-1] - self.ambient_temperature) / (-1.0 * self.bc_thermal_conductivity)
+        
+        # Use the temperature field using forward Euler method
+        self.temperature_grid = self.temperature_grid + temperature_grid_rate * self.temporal_precision
+        
+        # Apply trigger thermal input
+        if self.current_time >= self.trigger_time and self.current_time < self.trigger_time + self.trigger_length:
+            self.temperature_grid[0] = self.trigger_temperature
+        
+        # Return the current state (reduced temperature field, front pos, front rate, input pos, input mag), and get reward
+        state = np.concatenate((self.temperature_grid[0::10], [self.front_position], [self.front_rate], [self.input_location], [self.input_magnitude]))
+        reward = self.get_reward(ok_action)
+        
+        # Update the current time and check for simulation completion
+        done = (self.current_time + 2.0*self.temporal_precision >= self.simulation_time)
+        if not done:
+            self.current_time = self.current_time + self.temporal_precision
+            
+        # Return next state, reward for previous action, and whether simulation is complete or not
+        return state, reward, done
+    
+    def get_reward(self, ok_action):
+        
+        if not ok_action:
+            return -self.max_reward
+        
+        else:
+            
+            # If the maximum temperature is below the maximum allowed temperature, return a positive reward
+            if (self.temperature_grid <= self.maximum_temperature).all():
+                
+                # Calculate square error between current state and desired state
+                sq_error = ((self.front_rate - self.desired_front_rate)**2)
+                reward = max(self.max_reward - self.c1 * sq_error, self.min_positive_reward)
+                
+            # If the maximum temperature is above the maximum allowed temperature, return a negative reward
+            else:
+                reward = -self.c2 * self.max_reward
+            
+            return reward
+    
+    def reset(self):
+        # Reset time
+        self.current_time = 0.0
+        
+        # Reset input
+        self.input_location = self.initial_input_location
+        self.input_magnitude = self.peak_thermal_rate / 2.0
+                
+        # Reset fields
+        self.temperature_grid = np.array([self.initial_temperature]*self.spacial_precision)
+        self.cure_grid = np.array([self.initial_cure]*self.spacial_precision)
+        self.input_grid = np.array([0.0]*self.spacial_precision)
+        self.front_position = 0.0
+        
+        # Return the temperature field
+        return np.concatenate((self.temperature_grid[0::10], [self.front_position], [0.0], [self.input_location], [self.input_magnitude]))
---
+++ 1D_Cure/PPO_Agent.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 11 10:41:07 2020
+
+@author: Grayson Schaer, gschaer2
+"""
+
+import numpy as np
+import torch
+import NN_Stdev
+import NN
+
+class PPO_Agent:
+    
+    def __init__(self, num_states, steps_per_trajectory, trajectories_per_batch, minibatch_size, num_epochs, gamma, lamb, epsilon, alpha):
+
+        # Policy and value estimation network
+        # Input is the state
+        # Output is the mean of the gaussian distribution from which actions are sampled
+        self.actor = NN_Stdev.Neural_Network(num_inputs=num_states, num_outputs=1, num_hidden_layers=2, num_neurons_in_layer=128)
+        self.actor_optimizer =  torch.optim.Adam(self.actor.parameters() , lr=alpha)
+        
+        # Old policy and value estimation network used to calculate clipped surrogate objective
+        # Input is the state
+        # Output is the mean of the gaussian distribution from which actions are sampled
+        self.old_actor = NN_Stdev.Neural_Network(num_inputs=num_states, num_outputs=1, num_hidden_layers=2, num_neurons_in_layer=128)
+        self.old_actor.load_state_dict(self.actor.state_dict())
+                
+        # Critic NN that estimates the value function
+        # Input is the state
+        # Output is the estimated value of that state
+        self.critic =  NN.Neural_Network(num_inputs=num_states, num_outputs=1, num_hidden_layers=2, num_neurons_in_layer=128)
+        self.critic_optimizer =  torch.optim.Adam(self.critic.parameters() , lr=alpha)
+        
+        # Trajectory memory
+        self.num_states = num_states
+        self.steps_per_trajectory = steps_per_trajectory
+        self.trajectory_index  = 0
+        self.trajectories_per_batch = trajectories_per_batch
+        self.trajectory_states = np.zeros((trajectories_per_batch*steps_per_trajectory, num_states))
+        self.trajectory_actions = np.zeros(trajectories_per_batch*steps_per_trajectory)
+        self.trajectory_rewards = np.zeros(trajectories_per_batch*steps_per_trajectory)
+        
+        # Hyperparameters
+        self.minibatch_size = minibatch_size
+        self.num_epochs = num_epochs
+        self.gamma = gamma
+        self.lamb = lamb
+        self.epsilon = epsilon
+        self.gamma_lamb_reduction_array= np.zeros(steps_per_trajectory)
+        for curr_step in range(steps_per_trajectory):
+            self.gamma_lamb_reduction_array[curr_step] = (self.gamma * self.lamb) ** (curr_step)
+        
+        # Training memory
+        self.value_estimation_error = []
+        self.deltas_memory = []
+        
+    # Clips an input float to the range [min_val, max_val]
+    # @ param num - the float to be clipped
+    # @ param min_val - the minimal inclusive value num can take
+    # @ param max_val - the maximum inclusive value num can take
+    # @ return - the clipped float
+    def clip_float(self, num, min_val, max_val):
+        return max(min(num, max_val), min_val)
+    
+    
+    # Calcuates action given state and policy.
+    # @ param state - The state in which the policy is applied to calculate the action
+    # @ return action - The calculated action based on the state and policy
+    # @ return stdev - The calculated stdev based on the policy
+    def get_action(self, state):
+        
+        # Get the gaussian distribution parameters used to sample the action for the old and new policy
+        mean, stdev = self.actor.forward(torch.tensor(state, dtype=torch.float))
+        
+        # Sample the action and constrain it to the space [0.0, 1.0]
+        dist = torch.distributions.normal.Normal(mean, stdev)
+        action = dist.sample().item()
+        stdev = stdev.detach().item()
+        
+        return action, stdev
+  
+    
+    # Gets the autodifferentiable probability ratios for the given trajectory
+    # @ param minibatch_indices - list of indices that represent set of minibatches over which SGD will occur
+    # @ return - the differentiable probability ratios
+    def get_action_prob_ratio_minibatches(self, minibatch_indices):
+        
+        probability_ratio_minibatches = []
+        
+        for curr_epoch in range(self.num_epochs):
+            # Get the current and old action distribution parameters
+            state_minibatch = torch.tensor(self.trajectory_states[minibatch_indices[curr_epoch,:], :], dtype=torch.float)
+            mean_minibatch, stdev = self.actor.forward(state_minibatch)
+            with torch.no_grad():
+                old_mean_minibatch, old_stdev = self.old_actor.forward(state_minibatch)
+            
+            # Set the distributions based on the parameters above
+            dist = torch.distributions.normal.Normal(mean_minibatch.squeeze(), stdev)
+            old_dist = torch.distributions.normal.Normal(old_mean_minibatch.squeeze(), old_stdev)
+            
+            # Get the probability ratios
+            action_minibatch = torch.tensor(self.trajectory_actions[minibatch_indices[curr_epoch,:]], dtype=torch.float)
+            numerator = torch.exp(dist.log_prob(action_minibatch)).double()
+            denominator =  torch.exp(old_dist.log_prob(action_minibatch)).double()
+            probability_ratio = numerator / denominator
+            
+            probability_ratio_minibatches.append(probability_ratio)
+            
+        return probability_ratio_minibatches
+    
+    
+    # Calculates the target value function of a given state at time t based on the TD(0) algorithm
+    # @ param minibatch_indices - list of indices that represent set of minibatches over which SGD will occur
+    # @ return - value targets for the minibatches
+    def get_value_target_minibatches(self, minibatch_indices):
+                
+        value_target_minibatches = []
+        
+        states = torch.tensor(self.trajectory_states, dtype=torch.float)
+        with torch.no_grad():
+            next_value_estimate_minibatch = self.critic.forward(states).double().squeeze()
+        next_minibatch_indices = minibatch_indices + 1
+        mask = next_minibatch_indices % self.steps_per_trajectory == 0
+        next_minibatch_indices[mask] = next_minibatch_indices[mask] - 1
+        
+        for curr_epoch in range(self.num_epochs):
+            # Calculate V(s_{t+1})
+            next_value_estimate = next_value_estimate_minibatch[next_minibatch_indices[curr_epoch]]
+            next_value_estimate[mask[curr_epoch]] = torch.tensor(self.trajectory_rewards[(minibatch_indices)[curr_epoch,mask[curr_epoch]]]).double()
+            
+            # Use the TD(0) algorithm to calculate the target value function
+            reward = torch.tensor(self.trajectory_rewards[minibatch_indices[curr_epoch]])
+            value_target = reward + self.gamma * next_value_estimate
+            value_target_minibatches.append(value_target)
+        
+        return value_target_minibatches
+  
+    
+    # Estimates the value function of a given state trajectory based on the critic NN
+    # @ param minibatch_indices - list of indices that represent set of minibatches over which SGD will occur
+    # @ return - value estimation of the stored states and the value estimation of the next stored states
+    def get_value_estimate_minibatches(self, minibatch_indices):
+        
+        value_estimate_minibatches = []
+        
+        for curr_epoch in range(self.num_epochs):
+            # Get the value estimation based on the critic NN
+            state_minibatch = torch.tensor(self.trajectory_states[minibatch_indices[curr_epoch,:], :], dtype=torch.float)
+            value_estimate = self.critic.forward(state_minibatch).double().squeeze()
+            value_estimate_minibatches.append(value_estimate)
+            
+        return value_estimate_minibatches
+        
+    
+    # Estimates the value function of a given state trajectory based on the critic NN
+    # @ return - value estimation of the stored states
+    def get_value_estimate(self):
+        
+        # Get the value estimation based on the critic NN
+        state = torch.tensor(self.trajectory_states, dtype=torch.float)
+        value_estimate = self.critic.forward(state).double().detach().numpy().reshape(self.trajectories_per_batch,self.steps_per_trajectory)
+        
+        return value_estimate
+    
+    
+    # Calculates the advantage function sequence for a given trajectory
+    # @ param minibatch_indices - list of indices that represent set of minibatches over which SGD will occur
+    # @ return - sequence of advantage function values based on GAE algrothim
+    def get_advantage_minibatches(self, minibatch_indices):
+        
+        # Split rewards and value estimates into their respective trajecotries
+        value_estimates = self.get_value_estimate()
+        rewards = self.trajectory_rewards.reshape(self.trajectories_per_batch,self.steps_per_trajectory)
+        
+        # Calculate the deltas
+        gamma_reduced_next_value_estimates = self.gamma * np.roll(value_estimates, -1, axis=1)
+        deltas = rewards + gamma_reduced_next_value_estimates - value_estimates
+        deltas[:,-1] = rewards[:,-1]
+        self.deltas_memory.append(np.sqrt(np.mean(deltas**2)))
+        
+        # Initialize the advantage array
+        advantages = np.zeros((self.trajectories_per_batch,self.steps_per_trajectory))
+        
+        # Use deltas to find the advantage function values by GAE
+        for curr_step in range(self.steps_per_trajectory):
+            deltas_subset = deltas[:, curr_step:]
+            gamma_lamb_reduction_subset = self.gamma_lamb_reduction_array[0:(self.steps_per_trajectory - curr_step)]
+            advantages[:, curr_step] = np.sum(deltas_subset * gamma_lamb_reduction_subset, axis=1)
+        
+        # Format the output
+        advantages = torch.tensor(advantages.reshape(self.trajectories_per_batch*self.steps_per_trajectory)).double()
+        
+        advantage_estimate_minibatches = []
+        for curr_epoch in range(self.num_epochs):
+            advantage_estimate_minibatches.append(advantages[minibatch_indices[curr_epoch]])
+        
+        return advantage_estimate_minibatches
+        
+    
+    # Updates the trajectory memory given an arbitrary time step
+    # @ param state - state to be added to trajectory memory
+    # @ param action - action to be added to trajectory memory
+    # @ param reward - reward to be added to trajectory memory
+    def update_agent(self, state, action, reward):
+        
+        # Calculate the location in the memory matrices to insert new data
+        current_trajectory = self.trajectory_index // self.steps_per_trajectory
+        current_step = self.trajectory_index - current_trajectory * self.steps_per_trajectory
+        
+        # Update the state, action, and reward memory
+        self.trajectory_states[self.trajectory_index,:] = state 
+        self.trajectory_actions[self.trajectory_index] = action
+        self.trajectory_rewards[self.trajectory_index] = reward
+        
+        # Update the trajectory index
+        self.trajectory_index = self.trajectory_index + 1
+            
+        # If a trajectory batch is complete, apply the learning algorithm
+        if current_step == self.steps_per_trajectory - 1 and current_trajectory == self.trajectories_per_batch - 1:
+            
+            # Define the minibatches to be used for minibatch SGD
+            minibatch_indices = np.random.permutation(len(self.trajectory_actions)).astype(int).reshape(self.num_epochs, self.minibatch_size)
+            
+            # Calculate the value estimates and targets in minibatches
+            value_estimate_minibatches = self.get_value_estimate_minibatches(minibatch_indices)
+            value_target_minibatches =  self.get_value_target_minibatches(minibatch_indices)
+            
+            # Calculate the advantage estimates and probability ratios in minibatches
+            advantage_estimate_minibatches = self.get_advantage_minibatches(minibatch_indices)
+            probability_ratio_minibatches = self.get_action_prob_ratio_minibatches(minibatch_indices)
+            
+            # Copy the current state dictionary to the old actor and let the actor and critic learn
+            self.old_actor.load_state_dict(self.actor.state_dict())
+            self.learn(advantage_estimate_minibatches, probability_ratio_minibatches, value_estimate_minibatches, value_target_minibatches)
+            
+            # After learning, reset the memory
+            self.trajectory_index  = 0
+            self.trajectory_states = np.zeros((self.trajectories_per_batch*self.steps_per_trajectory, self.num_states))
+            self.trajectory_actions = np.zeros(self.trajectories_per_batch*self.steps_per_trajectory)
+            self.trajectory_rewards = np.zeros(self.trajectories_per_batch*self.steps_per_trajectory)
+        
+        
+    # Updates the actor's NN weights based on a batch of trajectories
+    # @ param advantage_estimate_minibatches - non differentiable minibatch set of advantage estimates
+    # @ param probability_ratio_minibatches - differentiable minibatch set of action probability ratios
+    # @ param value_estimate_minibatches - differentiable minibatch set of value estimates
+    # @ param value_target_minibatches - non differentiable minibatch set of value targets
+    def learn(self, advantage_estimate_minibatches, probability_ratio_minibatches, value_estimate_minibatches, value_target_minibatches):
+        
+        actor_loss = 0.0
+        critic_loss = 0.0
+        total_target = 0.0
+        total_value = 0.0
+                                
+        for curr_epoch in range(self.num_epochs):
+            # Apply the clipped surrogate objective algorithm
+            surrogate_objective = (advantage_estimate_minibatches[curr_epoch] * probability_ratio_minibatches[curr_epoch])
+            clipped_objective = (probability_ratio_minibatches[curr_epoch].clamp(1-self.epsilon, 1+self.epsilon) * advantage_estimate_minibatches[curr_epoch])
+            
+            # Define CSO
+            clipped_surrogate_objective = torch.min(surrogate_objective, clipped_objective)
+            
+            # Calculate the loss function for the value estimation
+            squared_error_loss = (value_target_minibatches[curr_epoch] - value_estimate_minibatches[curr_epoch])**2
+            
+            # Calculate the total loss function
+            actor_loss = actor_loss - (clipped_surrogate_objective).mean()
+            critic_loss = critic_loss + (squared_error_loss).mean()
+            
+            total_target = total_target + value_target_minibatches[curr_epoch].mean().item()
+            total_value = total_value + value_estimate_minibatches[curr_epoch].mean().item()
+            
+        self.value_estimation_error.append(100.0*(total_target - total_value)/total_target)
+            
+        # Conduct minibatch stochastic gradient descent on actor
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+        
+        # Conduct minibatch stochastic gradient descent on critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        
