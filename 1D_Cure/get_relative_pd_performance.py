fad29cbbbb277e53d5f927fab3378b9e663f4491
GrayKS3248 <gschaer2@illinois.edu>
2020-12-26 14:16:50 -0600

    modified save paths in get_relative_pd_performance.py

--- 1D_Cure/get_relative_pd_performance.py
+++ 1D_Cure/get_relative_pd_performance.py
@@ -10,51 +10,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
-with open("results/pd_05/output", 'rb') as file:
-    pd_299 = pickle.load(file)
 with open("results/pd_1/output", 'rb') as file:
-    pd_300 = pickle.load(file)
-with open("results/pd_15/output", 'rb') as file:
-    pd_301 = pickle.load(file)
+    pd_1 = pickle.load(file)
 with open("results/pd_2/output", 'rb') as file:
-    pd_302 = pickle.load(file)
-with open("results/pd_25/output", 'rb') as file:
-    pd_303 = pickle.load(file)
+    pd_2 = pickle.load(file)
 with open("results/pd_3/output", 'rb') as file:
-    pd_304 = pickle.load(file)
-with open("results/pd_35/output", 'rb') as file:
-    pd_305 = pickle.load(file)
+    pd_3 = pickle.load(file)
 with open("results/pd_4/output", 'rb') as file:
-    pd_306 = pickle.load(file)
+    pd_4 = pickle.load(file)
+with open("results/pd_5/output", 'rb') as file:
+    pd_5 = pickle.load(file)
+with open("results/pd_6/output", 'rb') as file:
+    pd_6 = pickle.load(file)
+with open("results/pd_7/output", 'rb') as file:
+    pd_7 = pickle.load(file)
+with open("results/pd_8/output", 'rb') as file:
+    pd_8 = pickle.load(file)
     
 # Process data
-pd_299_mean = np.mean(np.array(pd_299['logbook']['data'][0]['r_per_episode']))
-pd_299_stdev = np.std(np.array(pd_299['logbook']['data'][0]['r_per_episode']))
-pd_300_mean = np.mean(np.array(pd_300['logbook']['data'][0]['r_per_episode']))
-pd_300_stdev = np.std(np.array(pd_300['logbook']['data'][0]['r_per_episode']))
-pd_301_mean = np.mean(np.array(pd_301['logbook']['data'][0]['r_per_episode']))
-pd_301_stdev = np.std(np.array(pd_301['logbook']['data'][0]['r_per_episode']))
-pd_302_mean = np.mean(np.array(pd_302['logbook']['data'][0]['r_per_episode']))
-pd_302_stdev = np.std(np.array(pd_302['logbook']['data'][0]['r_per_episode']))
-pd_303_mean = np.mean(np.array(pd_303['logbook']['data'][0]['r_per_episode']))
-pd_303_stdev = np.std(np.array(pd_303['logbook']['data'][0]['r_per_episode']))
-pd_304_mean = np.mean(np.array(pd_304['logbook']['data'][0]['r_per_episode']))
-pd_304_stdev = np.std(np.array(pd_304['logbook']['data'][0]['r_per_episode']))
-pd_305_mean = np.mean(np.array(pd_305['logbook']['data'][0]['r_per_episode']))
-pd_305_stdev = np.std(np.array(pd_305['logbook']['data'][0]['r_per_episode']))
-pd_306_mean = np.mean(np.array(pd_306['logbook']['data'][0]['r_per_episode']))
-pd_306_stdev = np.std(np.array(pd_306['logbook']['data'][0]['r_per_episode']))
+pd_1_mean = np.mean(np.array(pd_1['logbook']['data'][0]['r_per_episode']))
+pd_1_stdev = np.std(np.array(pd_1['logbook']['data'][0]['r_per_episode']))
+pd_2_mean = np.mean(np.array(pd_2['logbook']['data'][0]['r_per_episode']))
+pd_2_stdev = np.std(np.array(pd_2['logbook']['data'][0]['r_per_episode']))
+pd_3_mean = np.mean(np.array(pd_3['logbook']['data'][0]['r_per_episode']))
+pd_3_stdev = np.std(np.array(pd_3['logbook']['data'][0]['r_per_episode']))
+pd_4_mean = np.mean(np.array(pd_4['logbook']['data'][0]['r_per_episode']))
+pd_4_stdev = np.std(np.array(pd_4['logbook']['data'][0]['r_per_episode']))
+pd_5_mean = np.mean(np.array(pd_5['logbook']['data'][0]['r_per_episode']))
+pd_5_stdev = np.std(np.array(pd_5['logbook']['data'][0]['r_per_episode']))
+pd_6_mean = np.mean(np.array(pd_6['logbook']['data'][0]['r_per_episode']))
+pd_6_stdev = np.std(np.array(pd_6['logbook']['data'][0]['r_per_episode']))
+pd_7_mean = np.mean(np.array(pd_7['logbook']['data'][0]['r_per_episode']))
+pd_7_stdev = np.std(np.array(pd_7['logbook']['data'][0]['r_per_episode']))
+pd_8_mean = np.mean(np.array(pd_8['logbook']['data'][0]['r_per_episode']))
+pd_8_stdev = np.std(np.array(pd_8['logbook']['data'][0]['r_per_episode']))
 
 # Plot data
-objects = ('0.5', '1.0', '1.5', '2.0', '2.5', '3.0', '3.5', '4.0')
+objects = ('1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0')
 y_pos = np.arange(len(objects))
-performance = [pd_299_mean, pd_300_mean, pd_301_mean, pd_302_mean, pd_303_mean, pd_304_mean, pd_305_mean, pd_306_mean]
-error = [pd_299_stdev, pd_300_stdev, pd_301_stdev, pd_302_stdev, pd_303_stdev, pd_304_stdev, pd_305_stdev, pd_306_stdev]
+performance = [pd_1_mean, pd_2_mean, pd_3_mean, pd_4_mean, pd_5_mean, pd_6_mean, pd_7_mean, pd_8_mean]
+error = [pd_1_stdev, pd_2_stdev, pd_3_stdev, pd_4_stdev, pd_5_stdev, pd_6_stdev, pd_7_stdev, pd_8_stdev]
 plt.barh(y_pos, performance, xerr=error, capsize=5.0, align='center')
 plt.yticks(y_pos, objects)
 plt.xlabel('Average Reward per Simulation Step')
-plt.ylabel('K_P for Temperature Controller')
-plt.title('PD Controller Relative Performance with Random Target Front Velocity, N=500')
+plt.ylabel('Performance Parameter')
+plt.title('PD Controller Relative Performance, N=500')
 plt.gcf().set_size_inches(8.5, 5.5)
-plt.savefig('results/pd_results/kp-temp_performance.png', dpi = 500)
+plt.savefig('results/PD_Results/performance.png', dpi = 500)
 plt.close()
