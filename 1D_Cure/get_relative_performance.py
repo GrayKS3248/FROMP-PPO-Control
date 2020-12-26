a39daf7c2ea6f38bca39c4da3d9c1659de69576d
GrayKS3248 <gschaer2@illinois.edu>
2020-12-26 14:11:43 -0600

    Modified save paths in get_relatice_performance.py

--- 1D_Cure/get_relative_performance.py
+++ 1D_Cure/get_relative_performance.py
@@ -10,27 +10,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load data
-with open("results/ppo_results/output", 'rb') as file:
+with open("results/PPO-Results/output", 'rb') as file:
     ppo = pickle.load(file)
-with open("results/pd_1/output", 'rb') as file:
-    pd_1 = pickle.load(file)
-with open("results/control/output", 'rb') as file:
+with open("results/PD-Controller/output", 'rb') as file:
+    pd = pickle.load(file)
+with open("results/Control/output", 'rb') as file:
     con = pickle.load(file)
     
 # Process data
 ppo_episode_rewards = []
-pd_1_episode_rewards = []
+pd_episode_rewards = []
 con_episode_rewards = []
 for current_agent in range(len(ppo['logbook']['agents'])):
     ppo_episode_rewards.append(ppo['logbook']['data'][current_agent]['r_per_episode'])
-    pd_1_episode_rewards.append(pd_1['logbook']['data'][current_agent]['r_per_episode'])
+    pd_episode_rewards.append(pd['logbook']['data'][current_agent]['r_per_episode'])
     con_episode_rewards.append(con['logbook']['data'][current_agent]['r_per_episode'])
 ppo_episode_rewards = np.array(ppo_episode_rewards)
 ppo_mean = np.mean(ppo_episode_rewards)
 ppo_stdev = np.std(ppo_episode_rewards)
-pd_1_episode_rewards = np.array(pd_1_episode_rewards)
-pd_1_mean = np.mean(pd_1_episode_rewards)
-pd_1_stdev = np.std(pd_1_episode_rewards)
+pd_episode_rewards = np.array(pd_episode_rewards)
+pd_mean = np.mean(pd_episode_rewards)
+pd_stdev = np.std(pd_episode_rewards)
 con_episode_rewards = np.array(con_episode_rewards)
 con_mean = np.mean(con_episode_rewards)
 con_stdev = np.std(con_episode_rewards)
@@ -38,8 +38,8 @@ con_stdev = np.std(con_episode_rewards)
 # Plot data
 objects = ('Control', 'PD', 'PPO')
 y_pos = np.arange(len(objects))
-performance = [con_mean, pd_1_mean, ppo_mean]+1.01*abs(con_mean)
-error = [con_stdev, pd_1_stdev, ppo_stdev]
+performance = [con_mean, pd_mean, ppo_mean]+1.01*abs(con_mean)
+error = [con_stdev, pd_stdev, ppo_stdev]
 plt.barh(y_pos, performance, xerr=error, capsize=5.0, align='center')
 plt.yticks(y_pos, objects)
 plt.xlabel('Average Reward per Simulation Step')
