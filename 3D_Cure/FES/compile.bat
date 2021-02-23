c7eca40ad4bd5cab6eb27fc7e83f2dceab198709
GrayKS3248 <gschaer2@illinois.edu>
2021-02-23 03:09:21 -0600

    changed the optimizer

--- 3D_Cure/FES/compile.bat
+++ 3D_Cure/FES/compile.bat
@@ -1,2 +1,2 @@
-g++ -fopenmp -Wall -O3 Finite_Element_Solver.h Finite_Element_Solver.cpp main.cpp -o FES
+g++ -fopenmp -Wall -O2 Finite_Element_Solver.h Finite_Element_Solver.cpp main.cpp -o FES
 pause
